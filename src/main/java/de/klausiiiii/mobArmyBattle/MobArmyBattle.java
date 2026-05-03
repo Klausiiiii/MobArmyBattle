16cf1d97d71ee54c1757e1d736dce5bf6b62c2f7
Klausiiiii <quackel0@gmail.com>
2026-05-03 11:42:47 +0200

    feat: wire MatchManager and /mab command in plugin lifecycle

--- src/main/java/de/klausiiiii/mobArmyBattle/MobArmyBattle.java
+++ src/main/java/de/klausiiiii/mobArmyBattle/MobArmyBattle.java
@@ -1,16 +1,35 @@
 package de.klausiiiii.mobArmyBattle;
 
+import de.klausiiiii.mobArmyBattle.command.MabCommand;
+import de.klausiiiii.mobArmyBattle.match.MatchManager;
+import org.bukkit.command.PluginCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MobArmyBattle extends JavaPlugin {
 
+    private MatchManager matchManager;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        matchManager = new MatchManager();
+
+        PluginCommand mabCmd = getCommand("mab");
+        if (mabCmd == null) {
+            getLogger().severe("Befehl /mab nicht in plugin.yml deklariert!");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+        mabCmd.setExecutor(new MabCommand(matchManager));
+
+        getLogger().info("MobArmyBattle aktiviert.");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info("MobArmyBattle deaktiviert.");
+    }
+
+    public MatchManager getMatchManager() {
+        return matchManager;
     }
 }
