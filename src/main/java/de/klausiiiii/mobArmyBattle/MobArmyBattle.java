ee8d11b77c63818501955391c23449482aabd87d
Klausiiiii <quackel0@gmail.com>
2026-05-03 09:52:30 +0200

    Init

---
+++ src/main/java/de/klausiiiii/mobArmyBattle/MobArmyBattle.java
@@ -0,0 +1,16 @@
+package de.klausiiiii.mobArmyBattle;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MobArmyBattle extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
