5b7f5fdba41d7030364e7a24831a250b9c8c542f
Klausiiiii <quackel0@gmail.com>
2026-05-09 14:22:48 +0200

    feat(listener): clean up spectator state on player quit

--- src/main/java/de/klausiiiii/mobArmyBattle/listener/PlayerConnectionListener.java
+++ src/main/java/de/klausiiiii/mobArmyBattle/listener/PlayerConnectionListener.java
@@ -1,6 +1,7 @@
 package de.klausiiiii.mobArmyBattle.listener;
 
 import de.klausiiiii.mobArmyBattle.match.MatchManager;
+import de.klausiiiii.mobArmyBattle.spectator.SpectatorManager;
 import de.klausiiiii.mobArmyBattle.tournament.TournamentManager;
 import de.klausiiiii.mobArmyBattle.world.WorldManager;
 import org.bukkit.event.EventHandler;
@@ -9,17 +10,27 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import java.util.UUID;
+
 public class PlayerConnectionListener implements Listener {
 
     private final MatchManager matchManager;
     private final WorldManager worldManager;
     private final TournamentManager tournamentManager;
+    private final SpectatorManager spectatorManager;
 
     public PlayerConnectionListener(MatchManager matchManager, WorldManager worldManager,
                                     TournamentManager tournamentManager) {
+        this(matchManager, worldManager, tournamentManager, null);
+    }
+
+    public PlayerConnectionListener(MatchManager matchManager, WorldManager worldManager,
+                                    TournamentManager tournamentManager,
+                                    SpectatorManager spectatorManager) {
         this.matchManager = matchManager;
         this.worldManager = worldManager;
         this.tournamentManager = tournamentManager;
+        this.spectatorManager = spectatorManager;
     }
 
     @EventHandler(priority = EventPriority.NORMAL)
@@ -32,9 +43,14 @@ public class PlayerConnectionListener implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL)
     public void onQuit(PlayerQuitEvent event) {
-        if (tournamentManager != null) {
-            tournamentManager.onCaptainQuit(event.getPlayer().getUniqueId());
+        UUID id = event.getPlayer().getUniqueId();
+        if (spectatorManager != null && spectatorManager.isSpectating(id)) {
+            spectatorManager.endSpectate(id);
+            return;
         }
-        matchManager.leaveMatch(event.getPlayer().getUniqueId());
+        if (tournamentManager != null) {
+            tournamentManager.onCaptainQuit(id);
+        }
+        matchManager.leaveMatch(id);
     }
 }
